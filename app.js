eba68952df77b5b86dcb6285589ce78256d232d2
Uladzislau Yakauleu <u.yalauleu@gmail.com>
2019-08-29 17:23:38 +0300

    update

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
-import { GraphQLServer } from 'graphql-yoga'
-import Query from '@/resolvers/Query'
-import Mutation from '@/resolvers/Mutation'
-import Subscription from '@/resolvers/Subscription'
+import express from 'express'
+import { ApolloServer, gql } from 'apollo-server-express'
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`
 
 const resolvers = {
-  Query,
-  Mutation,
-  Subscription,
+  Query: {
+    hello: () => 'Hello world!',
+  },
 }
 
-const server = new GraphQLServer({
-  typeDefs: './src/schema.graphql',
+const server = new ApolloServer({
+  typeDefs,
   resolvers,
 })
 
-server.start(() => console.log(`Server is running on http://localhost:4000`))
+const app = express()
+server.applyMiddleware({ app })
+
+app.listen({ port: 4000 }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`),
+)
