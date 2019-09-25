69923a3462c65481924ba3372f0625a6aba26e28
Uladzislau Yakauleu <u.yalauleu@gmail.com>
2019-09-25 17:07:46 +0300

    upload single or multi files

--- src/lib/core/upload.js
+++ src/lib/core/upload.js
@@ -1,4 +1,7 @@
 import fs from 'fs'
+import uuid from 'uuid'
+
+const whiteList = ['png', 'jpg', 'jpeg']
 
 export const staticFiles = async (req, res) => {
   const path = `${process.cwd()}/uploads/${req.params.file}`
@@ -8,4 +11,49 @@ export const staticFiles = async (req, res) => {
   }
 
   return res.sendFile(path)
+}
+
+export const singleUpload = (file, opts = { whiteList }) => {
+  return file.then(file => {
+    const stream = file.createReadStream()
+    const extension = file.mimetype.split('/')[1]
+
+    if (!whiteList.includes(extension)) {
+      throw new Error('No allowed extension')
+    }
+
+    const prefix = opts.prefix || ''
+    const path = `${process.cwd()}/uploads/${prefix}${uuid()}.${extension}`
+
+    if (!fs.existsSync(path)) {
+      fs.mkdirSync(`${process.cwd()}/uploads/${prefix}`)
+    }
+
+    stream.pipe(fs.createWriteStream(path))
+
+    return file
+  })
+}
+
+export const multiUpload = async (files, opts = { whiteList }) => {
+  return files.map(async raw => {
+    const file = await raw.then(data => data)
+    const stream = file.createReadStream()
+    const extension = file.mimetype.split('/')[1]
+
+    if (!whiteList.includes(extension)) {
+      throw new Error('No allowed extension')
+    }
+
+    const prefix = opts.prefix || ''
+    const path = `${process.cwd()}/uploads/${prefix}${uuid()}.${extension}`
+
+    if (!fs.existsSync(path)) {
+      fs.mkdirSync(`${process.cwd()}/uploads/${prefix}`)
+    }
+
+    stream.pipe(fs.createWriteStream(path))
+
+    return file
+  })
 }
--- src/modules/user/resolvers/Mutation.js
+++ src/modules/user/resolvers/Mutation.js
@@ -1,3 +1,5 @@
+import { singleUpload, multiUpload } from '@/lib'
+
 const createUser = async (root, { email, username, password }, { db }) => {
   const createdUser = await db.create({
     email,
@@ -8,6 +10,16 @@ const createUser = async (root, { email, username, password }, { db }) => {
   return createdUser
 }
 
+const upload = async (root, { file }, { db }) => {
+  return singleUpload(file, { prefix: 'avatars/' })
+}
+
+const uploadMany = async (root, { files }, { db }) => {
+  return multiUpload(files)
+}
+
 export const Mutation = {
   createUser,
+  upload,
+  uploadMany,
 }
