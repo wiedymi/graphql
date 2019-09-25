86a6844465474fed56231d9e101416eed797d069
Uladzislau Yakauleu <u.yalauleu@gmail.com>
2019-09-25 18:07:42 +0300

    validation

--- src/modules/upload/resolvers/Mutation.js
+++ src/modules/upload/resolvers/Mutation.js
@@ -1,14 +1,26 @@
+import { validator } from 'graphql-validation'
 import { singleUpload, multiUpload } from '@/lib'
+import { uploadValidator } from './validators'
 
-const upload = async (root, { file }) => {
+const upload = async (root, { file }, { validationErrors }) => {
+  if (validationErrors) {
+    throw new Error(validationErrors)
+  }
+
   return singleUpload(file, { prefix: 'avatars/' })
 }
 
-const uploadMany = async (root, { files }) => {
+const uploadMany = async (root, { files }, context) => {
+  if (context.validationErrors) {
+    console.log(context.validationErrors)
+
+    return
+  }
+
   return multiUpload(files)
 }
 
 export const Mutation = {
-  upload,
-  uploadMany,
+  upload: validator(uploadValidator, upload),
+  uploadMany: validator([], uploadMany),
 }
